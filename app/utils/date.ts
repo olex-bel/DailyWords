31cd6424980e53d02b3662fe79e8183e6a6da053
ban_konung <ban_konung@ukr.net>
2026-02-08 19:00:27 +0100

    Fixed issue, when the empty list of words is returned. Fixed smart review issue.

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -18,7 +18,7 @@ export function formatDate(date: Date): string {
 }
 
 export function sameDay(d1: Date, d2: Date) {
-    return d1.getUTCFullYear() == d2.getUTCFullYear() &&
-         d1.getUTCMonth() == d2.getUTCMonth() &&
-         d1.getUTCDate() == d2.getUTCDate();
+    return d1.getFullYear() == d2.getFullYear() &&
+         d1.getMonth() == d2.getMonth() &&
+         d1.getDate() == d2.getDate();
 }
